9233780e8b4a9af26edafa1c4932870ae3d01554
m-hachisu <test@example.com>
2023-01-30 07:49:12 +0000

    【PHP/Laravel】07_課題

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>profile編集画面</h1>
-    </body>
-</html>
+@extends('layouts.edit')
+
+@section('title', 'プロフィールの編集')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィールの編集</h2>
+            </div>
+        </div>
+    </div>
+@endsection
