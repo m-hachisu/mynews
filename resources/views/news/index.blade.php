de59a430c2e5d47b0bf3515bc67f1925ea605e30
m-hachisu <test@example.com>
2023-02-20 05:29:46 +0000

    Laravel_16_課題_追記

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -29,11 +29,11 @@
         <hr color="#c0c0c0">
         <div class="row">
             <div class="posts col-md-8 mx-auto mt-3">
+                <p>Laravel_16_課題_追記</p>
                 @foreach($posts as $post)
                     <div class="post">
                         <div class="row">
                             <div class="text col-md-6">
-                                <p>Laravel_16_課題</p>
                                 <div class="date">
                                     {{ $post->updated_at->format('Y年m月d日') }}
                                 </div>
